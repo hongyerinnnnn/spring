75d9f6f4a877659307a91836a43c2924974619cf
Hong Ye Rin <yerinnn526@naver.com>
2025-09-05 17:36:03 +0900

    spring

---
+++ src/main/java/com/multi/controller/HomeController.java
@@ -0,0 +1,22 @@
+package com.multi.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping({"/", "/home"})
+    public String home(Model model){
+        model.addAttribute("msg", "Spring Legacy (XML) running!");
+        return "home"; // /WEB-INF/views/home.jsp
+    }
+    @GetMapping("/message")
+    public String message(Model model){
+        model.addAttribute("msg", "Hello Spring");
+        return "show";
+    }
+}
+
---
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,10 @@
+<%@ page contentType="text/html; charset=UTF-8" %>
+<!DOCTYPE html>
+<html lang="ko">
+<head><meta charset="UTF-8"><title>Home</title></head>
+<body>
+<h1>${msg}</h1>
+<p>뷰 리졸버 정상 동작</p>
+</body>
+</html>
+
