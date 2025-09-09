0e9c9c19ddebbece2e56ee57a854e1a5bffdff35
Hong Ye Rin <yerinnn526@naver.com>
2025-09-09 15:23:28 +0900

    designpattern

--- src/main/java/com/multi/designpattern/ServiceMain.java
+++ src/main/java/com/multi/designpattern/ServiceMain.java
@@ -1,4 +1,33 @@
 package com.multi.designpattern;
 
+interface Service {
+    void execute();
+}
+
+class ServiceImpl implements Service {
+    @Override
+    public void execute() {
+        System.out.println("execute Service");
+    }
+}
+
+// 클라이언트 클래스
+class Client {
+    private final Service service;
+
+    // 생성자를 통한 의존성 주입
+    public Client(Service service) {
+        this.service = service;
+    }
+
+    public void performTask() {
+        service.execute();
+    }
+}
 public class ServiceMain {
+    public static void main(String[] args) {
+        Service service = new ServiceImpl();
+        Client client = new Client(service);
+        client.performTask();
+    }
 }
