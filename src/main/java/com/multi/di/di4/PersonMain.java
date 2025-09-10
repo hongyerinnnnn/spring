a6285a166fa8feddd2970ef48055be8fd1d13247
Hong Ye Rin <yerinnn526@naver.com>
2025-09-10 13:54:06 +0900

    di

--- src/main/java/com/multi/di/di4/PersonMain.java
+++ src/main/java/com/multi/di/di4/PersonMain.java
@@ -6,5 +6,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class PersonMain {
     public static void main(String[] args) {
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("/spring/persondi.xml");
+        PersonCollection  pCollection =((PersonCollection)applicationContext.getBean("pCollection"));
+        System.out.println(pCollection);
     }
 }
